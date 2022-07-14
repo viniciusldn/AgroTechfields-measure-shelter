daf2a1978764419d6062805764892eda1aa66028
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-07-14 11:57:44 -0300

    Add files via upload

---
+++ src/test/java/com/betrybe/wiproIT.java
@@ -0,0 +1,9 @@
+package com.betrybe;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class wiproIT extends wiproTest {
+
+    // Execute the same tests but in native mode.
+}
