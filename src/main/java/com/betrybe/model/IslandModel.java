2e761ac12927580429f3e1abf3e61efe18755eaf
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-07-15 10:45:49 -0300

    feat: IslandModel.java, cria arquivo e desenvolve lógica, para inserir informações das ilhas

--- src/main/java/com/betrybe/model/IslandModel.java
+++ src/main/java/com/betrybe/model/IslandModel.java
@@ -1,5 +1,48 @@
 package com.betrybe.model;
 
-public class IslandModel {
-    
+import org.bson.types.ObjectId;
+
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+
+@MongoEntity
+public class IslandModel extends PanacheMongoEntity {
+  
+  private ObjectId id;  
+  private String nome;
+  private String agriculture;
+  private boolean status;
+
+  public ObjectId getId() {
+    return id;
+  }
+
+  public void setId(ObjectId id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getAgriculture() {
+    return agriculture;
+  }
+
+  public void setAgriculture(String agriculture) {
+    this.agriculture = agriculture;
+  }
+
+  public Boolean getStatus() {
+    return status;
+  }
+
+  public void setStatus(Boolean status) {
+    this.status = status;
+  }
+
 }
