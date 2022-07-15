319655d8f44677bcf8b2493459e558e2103ec0a5
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-07-15 10:20:31 -0300

    feat: IslandUpdateDTO.java, cria arquivo e desenvolve lógica, para atualizar area da ilha

--- src/main/java/com/betrybe/dto/IslandUpdateDTO.java
+++ src/main/java/com/betrybe/dto/IslandUpdateDTO.java
@@ -1,5 +1,13 @@
 package com.betrybe.dto;
 
 public class IslandUpdateDTO {
-    
+  private Double area;
+
+  public Double getarea() {
+    return area;
+  }
+
+  public void setarea(Double area) {
+    this.area = area;
+  }
 }
