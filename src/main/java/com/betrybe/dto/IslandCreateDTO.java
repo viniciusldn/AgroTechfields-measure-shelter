11377ccb0e04cf3dc08afc459d9bce6d109d41ce
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-07-15 10:25:09 -0300

    feat: IslandCreateDTO.java, cria arquivo e desenvolve lógica, para inserir informações das ilhas

--- src/main/java/com/betrybe/dto/IslandCreateDTO.java
+++ src/main/java/com/betrybe/dto/IslandCreateDTO.java
@@ -1,5 +1,31 @@
 package com.betrybe.dto;
 
 public class IslandCreateDTO {
-    
+  private String nome;
+  private String agriculture;
+  private Double area;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getagriculture() {
+    return agriculture;
+  }
+
+  public void setagriculture(String agriculture) {
+    this.agriculture = agriculture;
+  }
+
+  public Double getarea() {
+    return area;
+  }
+
+  public void setarea(Double area) {
+    this.area = area;
+  }
 }
